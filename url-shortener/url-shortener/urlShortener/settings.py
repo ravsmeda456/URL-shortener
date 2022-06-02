12db49d1e16f135f6bc5b1feb101d75a8b787648
ravsmeda456 <ravsmeda456@gmail.com>
2022-06-02 14:34:24 +0530

    Update settings.py

--- url-shortener/url-shortener/urlShortener/settings.py
+++ url-shortener/url-shortener/urlShortener/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mx%f&_0+2vtt-v6w45c99*4^i)6n93_cauvw^7bi%fg5(oskpt'
+SECRET_KEY = '<<SECURITY_KEY>>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
